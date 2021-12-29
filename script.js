899b170c8355538a0bc6f398ef16ab071d61d0b7
Elisabeth Irgens <hello@elisabethirgens.com>
2021-12-29 12:17:45 +0100

    Remove registrations without a venue

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const resourcePath = "test-data.json";
 const awesomeList = document.querySelector("#awesome-list-of-places");
 
+function cleanUpData(rawData) {
+  // The raw data contains check-ins that are not of interest
+  // for example beers with no registered venue are cut away
+  newList = Array.from(rawData);
+  rawData.forEach(function (venue, index) {
+    if (venue.venue_name === null) {
+      newList.splice(index, 1);
+    }
+  });
+  // Pass this cleaned up list into createMarkup function
+  createMarkup(newList);
+}
+
 function createMarkup(everywhere) {
   // Loop through the array, spot by spot and create markup
   // This is currently local json data in a tiny test file,
@@ -30,8 +43,8 @@ function goAndFetchSomeData() {
       throw response.status;
     })
     .then(function (data) {
-      // Pass data into the createMarkup function
-      createMarkup(data);
+      // Pass data into the cleanUpData function
+      cleanUpData(data);
     })
     .catch(function (error) {
       console.warn(error);
