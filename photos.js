219ce5955791f7c15cecda925b5acd9fb98a6665
Elisabeth Irgens <hello@elisabethirgens.com>
2023-11-07 15:29:33 +0100

    Add function to download all my photos

--- photos.js
+++ photos.js
@@ -1,17 +1,41 @@
 const resourcePath = "test-data.json";
 //  const resourcePath = "raw-data-export.json";
 const theGallery = document.querySelector("#all-the-photos");
+const newList = [];
 
 function cleanUpData(rawData) {
   // The raw data contains check-ins without photos that are not of interest
-  const newList = [];
   rawData.forEach(function (item, index, array) {
     if (item.photo_url) {
       newList.push(item);
     }
   });
-  // Pass new cleaned up list into function to create markup for gallery
+
+  // Pass new cleaned up list into functions
   createGallery(newList);
+  // downloadPhotos(newList); // uncomment to call 🤠
+}
+
+// Not sure I need to use this function again, it might have done its job
+// https://elisabethirgens.github.io/notes/2023/11/free-my-photos/
+function downloadPhotos() {
+  newList.forEach(function (item, index) {
+    fetchWithAnchorElement(item, index);
+
+    async function fetchWithAnchorElement() {
+      const img = await fetch(item.photo_url);
+      console.log(item);
+      const imgBlob = await img.blob();
+      const imgUrl = URL.createObjectURL(imgBlob);
+
+      const anchor = document.createElement("a");
+      anchor.href = imgUrl;
+      anchor.download = `beer${index}.jpg`;
+      document.body.appendChild(anchor);
+      anchor.click();
+      document.body.removeChild(anchor);
+    }
+  });
 }
 
 function createGallery(everything) {
